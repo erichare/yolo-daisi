7973f3072ebe40cb156e8caf7cc27559a0cd33e9
Eric Hare <ericrhare@gmail.com>
2022-06-28 16:34:48 -0700

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -34,4 +34,13 @@ def yolo(source: pd.DataFrame="cat.jpeg"):
     return [{"type": "image", "label": "yolov6", "data":  {"alt": "yolov6 result", "src": "data:image/png;base64, " + im}}]
 
 if __name__ == "__main__":
-    st.title("Test")
+    st.title("Yolo V6 Model")
+
+    st.write("This Daisi allows you to provide an image, and one of the most advanced Object Detection algorithms available will try to classify it for you. Upload your data to get started!")
+    with st.sidebar:
+        uploaded_file = st.file_uploader("Choose a file")
+
+    print(uploaded_file)
+    res = yolo(uploaded_file)
+
+    st.markdown(res["data"]["src"], unsafe_allow_html=True)
